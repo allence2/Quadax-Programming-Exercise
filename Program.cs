4de9d78bd8a475560f101d9acd5ecd73b7959229
Chad Allen <allen687123@gmail.com>
2024-04-02 16:18:00 -0400

    Created Program class + Added Random Number Generage
    -Build Program.cs using Visual Studio's C# Dev Kit to utilize Dotnet package for debugging
    
    -Add method for generating a random number of length 4, digits only between 1, 6 inclusively

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+    class Program {
+
+
+        private static int generateRandomNumber() {
+            StringBuilder randomNumber = new StringBuilder("", 4);
+            Random rand = new Random();
+
+            // Generates a random digit beween 1 and 6 (Inclusively) for each digit
+            randomNumber.Append(rand.Next(1, 7));
+            randomNumber.Append(rand.Next(1, 7));
+            randomNumber.Append(rand.Next(1, 7));
+            randomNumber.Append(rand.Next(1, 7));
+
+            return Int32.Parse(randomNumber.ToString());
+        }
+
+        static int Main(string[] args) {
+            int tries = 10, answer = 0;
+
+            Console.Write(generateRandomNumber());
+
+            return 0;
+        }
+    }
