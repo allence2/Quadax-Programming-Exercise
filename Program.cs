43416a186e0b040c6bdab366f5590cdbb16d0ac0
Chad Allen <allen687123@gmail.com>
2024-04-02 16:48:43 -0400

    Add user input
    -Add user input and input verification that they have entered a number of length 4, and digits of 1 through 6

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Text;
 
     class Program {
 
-
+        /*Method for generating a random number where digits are beween 1 and 6 (Inclusively) for each digit */
         private static int generateRandomNumber() {
             StringBuilder randomNumber = new StringBuilder("", 4);
             Random rand = new Random();
@@ -18,10 +18,36 @@ using System.Text;
         }
 
         static int Main(string[] args) {
-            int tries = 10, answer = 0;
+            int tries = 10, count = 1;
+            int answer = generateRandomNumber();
 
-            Console.Write(generateRandomNumber());
+            while (count > 0) {
+                int userInput;
+                bool userValidInput = false;
 
+                // User input until they have given a valid number
+                do {
+                    Console.WriteLine("Enter as four digit number (Digits are 1-6): ");
+                    if(int.TryParse(Console.ReadLine(), out userInput) && userInput > 999 && userInput < 10000) {
+                        int temp = userInput;
+                        // Validation of the user's input that it is 4 digits in length and digits are between 1 and 6 inclusively
+                        for(int i = 0; i < 4; i++) {
+                            if(temp % 10 > 0 && temp % 10 < 7) {
+                                userValidInput = true;
+                                temp = temp / 10;
+                            } else {
+                                userValidInput = false;
+                                break;
+                            }
+                        }
+                    }
+                    if(!userValidInput) {
+                        Console.WriteLine("Incorrect number entered. Try Again.");
+                    }
+                } while (!userValidInput);
+
+                // Compare the two inputs
+            }; 
             return 0;
         }
     }
